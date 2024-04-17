9182c19c8540e1600c14024c61419e9e1ab25eb3
Bianchi-marina <141640079+Bianchi-marina@users.noreply.github.com>
2024-04-17 09:28:21 -0300

    fixed protectRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import Main from './Main';
 
 const ProtectedRoute = ({ component: Component, loggedIn, ...rest }) => {
   return (
     <Route
       {...rest}
       render={(props) => (
-        loggedIn ? <Main {...props} /> : <Redirect to="/signin" />
+        loggedIn ? <Component {...props} /> : <Redirect to="/signin" />
       )}
     />
   );
